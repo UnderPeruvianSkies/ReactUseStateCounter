b20bc5ecf10ca00b8e7b95b51d19e9b0bd578031
Harley Burrow <he.burrow@yahoo.co.uk>
2022-03-28 09:40:02 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+
+  };
+
+  return(
+    <div class="box">
+    <p>{counter}</p>
+    <button onClick={increment}>Increment</button>
     </div>
   );
-}
+};
 
 export default App;
